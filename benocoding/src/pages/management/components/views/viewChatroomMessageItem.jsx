5a7c6501a9b65342a6f603f0c78c840f14dc4e2f
SamFang <c.s.fangyolk@gmail.com>
2023-04-09 19:30:11 +0800

    style: improve accessibility and styling for chatroom messages
    - Add an accessible icon to each message item
    - Reformat the viewChatroomMessageItem component to improve the layout and styling
    
    Signed-off-by: SamFang <c.s.fangyolk@gmail.com>

--- benocoding/src/pages/management/components/views/viewChatroomMessageItem.jsx
+++ benocoding/src/pages/management/components/views/viewChatroomMessageItem.jsx
@@ -1,11 +1,39 @@
 
 import styled from "styled-components";
+import AccessibleForwardIcon from '@mui/icons-material/AccessibleForward';
 
 const MessageBlock = styled.div`
     font-size: 18px;
     text-align: left;
     letter-spacing: 2px;
-    padding: 0 0 15px 40px;
+    padding: 0 0 15px 0;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const MessageItem = styled.div`
+    height: 60px;
+    width: 100%;
+    margin-left: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const Username = styled.p`
+    font-size: 24px;
+    font-wieght: bold;
+    margin: 5px 0;
+    display: inline-block;
+`;
+
+const Time = styled.p`
+    font-size: 14px;
+    display: inline;
+    margin-left: 10px;
 `;
 
 const Message = styled.p`
@@ -22,7 +50,21 @@ const ViewChatroomMessageItem = ({msg}) => {
             <MessageBlock>
                 {
                     msgArray.map((text, idx) => {
-                        return <Message key={idx}>{text}</Message>
+                        return (
+                            <Wrapper>
+                                <AccessibleForwardIcon sx={{
+                                    height: "40px",
+                                    width: "40px"
+                                }}/>
+                                <MessageItem>
+                                    <Username>
+                                        {msg.from}
+                                        <Time>({msg.time})</Time>
+                                    </Username>
+                                    <Message key={idx}>{text}</Message>
+                                </MessageItem>
+                            </Wrapper>
+                        )
                     })
                 }
             </MessageBlock>
